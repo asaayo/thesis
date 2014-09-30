f86117f8638f594616022062cded9e268a774769
Brandon <Brandon@Lapbox.kutztown.edu>
2014-09-30 13:12:43 -0400

    Project initiation

---
+++ thesis/src/replaceQueries.py
@@ -0,0 +1,18 @@
+#  	Author: 		Brandon Trumble
+#	Purpose: 		Modify existing codebases that interact with databases
+#				to protect them from SQL injection
+#	Created: 		9/16/14
+#	Last Modified:          9/30/14
+#	
+#	Changes:		v 0.1: 		Created file
+#				v 0.11:		Begin work on replacing unsafe queries
+#
+
+
+
+def main():
+	print("The purpose of this file is twofold, to update existing queries to make them safer, and to put them through a filter so unsafe queries can be caught. 'n")
+	print("Enter the file which you would like to modify: ")
+
+if __name__ == '__main__':
+	main()

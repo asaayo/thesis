375fa69dd5d4e12f23ca71ae6e9c080120a7c2a4
Brandon <Brandon@Blackbox>
2014-11-03 16:48:57 -0500

    Queries are sorta cleaned up

--- thesis/src/replaceQueries.py
+++ thesis/src/replaceQueries.py
@@ -1,17 +1,21 @@
+# coding=utf-8
 #  	Author: 		Brandon Trumble
 #	Purpose: 		Modify existing codebases that interact with databases
 #				to protect them from SQL injection
 #	Created: 		9/16/14
-#	Last Modified:          10/15/14
+#	Last Modified:          11/03/14
 #	
 #	Changes:		v 0.1: 		Created file
 #				v 0.11:		Begin work on replacing unsafe queries
 #                               v 0.12:         Continue work on unsafe queries
+#                               v 0.13:         More unsafe query work
 #
+
 
 import sys
 import fileinput
 import shutil
+import re
 
 def main():
     print("The purpose of this file is twofold, to update existing queries to make them safer, and to put them through a filter so unsafe queries can be caught.")
@@ -24,20 +28,75 @@ def main():
     toMod = ""
     #Loop through the input file and get all the lines where SQL statements occur
     for line in fileinput.input(file, inplace=1):
-        print(line)
+        if "->prepare" in line:
+            print "Prepared statement detected, execution not needed"
+            exit(0)
+            
         if "SELECT" in line:
-            toMod += line
-            print(toMod)
-        if "DELETE" in line:
-            toMod += line
-            print(toMod)
-        if "INSERT" in line:
-            toMod += line
-            print(toMod)
-    print(toMod)
-
-def fixQueries(toMod):
-    print("fixQueries")
-
+            toMod = line
+            fix_queries(toMod)
+            
+        elif "DELETE" in line:
+            toMod = line
+            fix_queries(toMod)
+            
+        elif "INSERT" in line:
+            toMod = line
+            fix_queries(toMod)
+        else:
+            print line
+            
+#fix_queries is a function designed to fix unsafe queries in php
+#any time a line is detected that contains a SQL command word (INSERT, DELETE, SELECT)
+#it is passed into fix_queries to generated 
+def fix_queries(toMod):
+    #Regular expression to find PHP variables
+    pattern = re.compile('\$\w+')
+    #Return a list of php variables found as an iterator
+    #iterator containes a list of match objects
+    iter = pattern.finditer(toMod)
+    #Returns character location of PHP variable strings
+    for i in iter:
+        #debug to print the locations of phpvars
+        #print i.span()
+        #currentWord is the first phpvar token
+        #if it's a $mysqli-> statement, we want to let it through
+        currentWord = toMod[i.start(0):i.end(0)]
+        #debug to check that -> proceeds the first variable
+        #print toMod[i.end(0):i.end(0)+2]
+        if toMod[i.end(0):i.end(0)+2] == "->":
+            #have to use stdout to prevent line from breaking and a space from being appended
+            sys.stdout.write("$stmt = " + currentWord)
+            #if a string is proceeded by ->, it's probably a 
+            #mysqli statement, so we don't want to remove it
+            toMod=toMod[i.end(0):]
+    modded = re.sub('\$\w+','?',toMod)
+    
+    print modded
+    build_prepared_statement(toMod)
+    
+def build_prepared_statement(toMod):
+    
+    
+    pattern = re.compile('\$\w+')
+    iter = pattern.finditer(toMod)
+    
+    #Mind parameter types: s = string, i = integer, d = double, b = blob
+    sys.stdout.write("$stmt->bind_param(\'")
+    #no way to find the length of an iterator without iterating through it
+    for i in iter:
+        sys.stdout.write("s")
+    
+    sys.stdout.write("\'")
+    
+    iter = pattern.finditer(toMod)
+    for i in iter:
+        sys.stdout.write(" ," + toMod[i.start(0):i.end(0)])
+        
+    sys.stdout.write(")\n\n")
+    
+    sys.stdout.write("$stmt->execute()\n\n")
+    
+    
 if __name__ == '__main__':
     main()
